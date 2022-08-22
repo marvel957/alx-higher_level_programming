2ea393201f70c35f974517dca6ac0a8d1ed945dc
Marvel957 <hamsik957@gmail.com>
2022-08-22 08:24:28 -0700

    task 7

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 word = "Holberton"
-print(f"First 3 letters: {word[0:3]}")
-print(f"Last 2 letters: {word[7:]}")
-print(f"Middle word: {word[1:-1]}")
+a = word[0:3]
+b = word[7:]
+c = word[1:-1]
+print(f"First 3 letters: {a}")
+print(f"Last 2 letters: {b}")
+print(f"Middle word: {c}")
